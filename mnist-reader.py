3a1f0d1588484107ef4aa71e812139a286d99263
Keith Williams <G00324844@gmit.ie>
2017-09-23 09:42:10 +0100

    Convert the magic number from bytes to int.

--- mnist-reader.py
+++ mnist-reader.py
@@ -2,11 +2,19 @@
 # Date: 23/09/2017
 # Adapted from:
 #	https://stackoverflow.com/questions/12902540/read-from-a-gzip-file-in-python
+#	https://stackoverflow.com/questions/444591/convert-a-string-of-bytes-into-an-int-python
 
 import gzip
 
+# Open the gzip in read mode
 file = gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb')
 
-print(file.read(1))
+# Read the first four bytes i.e. the magic number
+magic = file.read(4)
+
+# Convert the magic number from bytes to an int
+magic = int.from_bytes(magic, byteorder='big')
+
+print(magic)
 
 file.close();
