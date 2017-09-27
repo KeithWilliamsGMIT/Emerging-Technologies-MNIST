a178070b878d9ec38eca0075ccb0ae2a36410885
Keith Williams <G00324844@gmit.ie>
2017-09-27 14:52:40 +0100

    Parse both data sets.

--- mnist-reader.py
+++ mnist-reader.py
@@ -4,5 +4,10 @@
 from models import MnistDataSet
 
 if __name__ == '__main__':
+	print("Parsing training set...")
+	train_set = MnistDataSet('data/train-images-idx3-ubyte.gz', 'data/train-labels-idx1-ubyte.gz')
+	train_set.parse_files()
+	
+	print("Parsing test set...")
 	test_set = MnistDataSet('data/t10k-images-idx3-ubyte.gz', 'data/t10k-labels-idx1-ubyte.gz')
 	test_set.parse_files()
